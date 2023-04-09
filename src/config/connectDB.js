6ad98a9b053d73779bea79eaf0f2aa4efdd9bc7f
Giáp Huy <20021366@vnu.edu.vn>
2023-04-09 22:34:33 +0700

    mongodb-clound

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 require("dotenv").config();
 
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGODB_URI, {
+  mongoose.connect(process.env.MONGODB_CLOUD_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });

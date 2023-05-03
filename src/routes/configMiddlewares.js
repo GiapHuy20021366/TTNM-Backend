4469c8637d4610634189d9a4432a6e026911e45a
Giáp Huy <20021366@vnu.edu.vn>
2023-05-03 23:30:50 +0700

    limit request to 50mb

--- src/routes/configMiddlewares.js
+++ src/routes/configMiddlewares.js
@@ -4,9 +4,9 @@ import bodyParser from "body-parser";
 import { middlewareStorage } from "../controllers/middlewares";
 
 const configMiddlewares = (app) => {
-  app.use(bodyParser.json());
+  app.use(bodyParser.json({ limit: "50mb" }));
   app.use(methodOverride("_method"));
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
   app.use(cors({ origin: true }));
   app.use(middlewareStorage);
 };
